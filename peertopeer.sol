054205b9fb57c701b64412c3205ae74bb52e1afa
Kartikey <37590463+A1kartikey@users.noreply.github.com>
2019-10-12 00:51:22 +0530

    Update peertopeer.sol

--- peertopeer.sol
+++ peertopeer.sol
@@ -1,19 +1,20 @@
 pragma solidity^0.5.1;
+
 contract peertopeerlending{
+    
     struct lender{
+        uint lbalance;
         string name;
         uint interest;
-        
-        uint lbalance;
         address Address;
         
     }
+    
     struct borrower{
         string name;
-        
         uint bbalance;
         address Address;
-          uint repay;
+        uint repay;
         
     }
     uint calculation;
@@ -23,47 +24,53 @@ contract peertopeerlending{
     
     address[] lenderacts;
     address[] borroweracts;
-    function enrollement(string memory _name,uint _interest,address _laddress,uint _amount)public{
+    
+    
+    function EnrollLender(string memory _name,uint _interest,address _laddress,uint _amount)public{
         lenders[_laddress].Address=_laddress;
         lenders[_laddress].name =_name;
         lenders[_laddress].interest=_interest;
         lenders[_laddress].lbalance =_amount;
         lenderacts.push(_laddress);
     }
-    function Enrollement(string memory _name,address  _baddress,uint _balance)public{
+    
+    function EnrollBorrower(string memory _name,address  _baddress,uint _balance)public{
         borrowers[_baddress]. Address=_baddress;
         borrowers[_baddress].name=_name;
         borrowers[_baddress].bbalance=_balance;
         borroweracts.push(_baddress);
         
-     
     }
     
-    function getlenderDetails(address _address) view public returns(uint){
-        return (lenders[_address].interest);
+    function GetLenderDetails(address _address) view public returns(string memory,address,uint,uint){
+        return (lenders[_address].name,lenders[_address].Address,lenders[_address].interest,lenders[_address].lbalance);
     }
-    function calculations(address _laddress,address _baddress, uint _amount,uint _time)public{
+    
+    function GetBorrowers(address _address)view public returns(string memory,address,uint,uint){
+        return(borrowers[_address].name,borrowers[_address].Address,borrowers[_address].bbalance,borrowers[_address].repay);
+    }
+    
+    function Loan(address _laddress,address _baddress, uint _amount,uint _time)public{
+        require((lenders[_laddress].lbalance > _amount) && (_laddress != _baddress));
         calculation = (lenders[_laddress].interest*_amount*_time)/100;
         lenders[_laddress].lbalance -= _amount ;
         borrowers[_baddress].bbalance += _amount;
-        borrowers[_baddress].repay =
-        calculation +_amount;
+        borrowers[_baddress].repay = calculation +_amount;
         
     }
-    function repay(address _laddress,address _baddress)  public {
+    
+    function Repay(address _laddress,address _baddress)  public {
      borrowers[_baddress].bbalance -= borrowers[_baddress].repay;
      lenders[_laddress].lbalance += borrowers[_baddress].repay;
-    } 
-    function getrepaydetails( address _address) view public  returns(uint){
-        return borrowers[_address].repay;
-    
     }
     
-    function balances(address _laddress,address _baddress)view public returns(uint,uint){
-        return(lenders[_laddress].lbalance,borrowers[_baddress].bbalance);
+    function GetAllBorrowers() public view returns(address[] memory ){
+        return borroweracts;
+    }
+    
+    function GetAllLenders() public view returns(address[] memory ){
+        return  lenderacts;
     }
 
-
-    
     
 }
