16f05968e8e628691c8f8d04ffa2307d7f494533
Kartikey <37590463+A1kartikey@users.noreply.github.com>
2019-12-03 23:56:04 +0530

    Update VotingSystem.sol

--- VotingSystem.sol
+++ VotingSystem.sol
@@ -1,39 +1,32 @@
-pragma solidity^0.4.19;
+pragma solidity^0.5.1;
 
 contract Vote{
     
-    address[]  candidateList;
+    uint[]  candidateList;
     
-      mapping (address => uint)  votesReceived;
-
-        address owner;
-
-        modifier ifOwner() {
-        require (owner == msg.sender);
-            _;
-        }
+      mapping (uint => uint)  votesReceived;
 
 
-    function List (address _address) public {
+    function List (uint _id) public {
         
-        candidateList.push(_address)  -1;
+        candidateList.push(_id) ;
     }
 
 
-    function printList()view  public returns (address[]){
+    function printList()view  public returns (uint[] memory){
         
         return candidateList;
     }    
     
-    function voteForCandidate(address candidate) public {
+    function voteForCandidate(uint candidate) public {
     votesReceived[candidate] += 1;
   }
 
-    function totalVotesFor(address candidate) view  public returns (uint) {
+    function totalVotesFor(uint candidate) view  public returns (uint) {
     return votesReceived[candidate];
   }
     
-    function validCandidate(address candidate) view  public returns (bool) {
+    function validCandidate(uint candidate) view  public returns (bool) {
     for(uint i = 0; i < candidateList.length; i++) {
       if (candidateList[i] == candidate) {
         return true;
@@ -43,5 +36,3 @@ contract Vote{
   }
     
 }
-
-
