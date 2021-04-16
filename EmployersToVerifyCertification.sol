5b050a32d135964f044e7f9c57efe4b9104cc325
Kartikey <37590463+A1kartikey@users.noreply.github.com>
2021-04-16 19:10:16 +0530

    Update EmployersToVerifyCertification.sol

--- EmployersToVerifyCertification.sol
+++ EmployersToVerifyCertification.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.5.1;
-import "browser/students.sol";//include the same file name
+pragma solidity ^0.8.3;
+import "./student.sol";//include the same file name
 contract Employer{  //contract name
-   studentDetails s1 = studentDetails (0xef55BfAc4228981E850936AAf042951F7b146e41);//s1 is an instance and copy the address of the imported file(Students_Univrsity.sol.sol)
+   studentDetails s1 = studentDetails (0xd2a5bC10698FD955D1Fe6cb468a17809A08fd005);//s1 is an instance and copy the address of the imported file(Students_Univrsity.sol.sol)
 constructor  () public {	//empty constructor
 	}
 function checkBgc(address vaddress) view public returns (string memory){
